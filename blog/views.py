6142e1d7398065e96b5b1749f4ae77d603989526
SawK0 <sawzawmyohtetko@gmail.com>
2022-07-18 17:26:53 +0700

    pagination

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+
+app_name = 'blog'
 
 def index(request):
     return render(request, 'blog/index.html')
+
+
+def post_list(request):
+    object_list = Post.objects.all()
+    paginator = Paginator(object_list, 3) # 3 posts per page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page':page, 'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, 
+                             status='published', 
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
